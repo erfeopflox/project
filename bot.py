8bd096da0570e97f2c42d60a53f2126309f01730
erfeopflox <erfeopflox@gmail.com>
2024-08-26 01:41:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("token")
+bot = telebot.TeleBot("7304730422:AAHY-ls0PKRV9unxsHvFN9c9lvPXC8qD2To")
 
-web_app_url = "t.me/hamstercombat_copy_bot/webapp"
+web_app_url = "t.me/freetokens_KT_bot/webapp"
 
-text = "Это приложение - копия гры Hamster Kombat, сделанная за 100 минут!\n\nНажмите на кнопку снизу, что запустить его!"
+text = "Это приложение - копия игры Hamster Kombat, сделанная за 100 минут!\n\nНажмите на кнопку снизу, что запустить его!"
 
 button = types.InlineKeyboardButton('Запустить', url=web_app_url)
 keyboard = types.InlineKeyboardMarkup()
